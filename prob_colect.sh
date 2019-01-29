3cc22c42856828133567d20b92e4df2ac471594a
orign-seven <45893917+orign-seven@users.noreply.github.com>
2019-01-29 09:03:54 +0800

    添加tcp分析local port部分

--- prob_colect.sh
+++ prob_colect.sh
@@ -69,7 +69,7 @@ disk_info(){
 tcp_info(){
     conn_info=`netstat -ntpa|grep -v 'LISTEN'| \
          awk '{print $4,$5,$6,$7}'|grep -P '^\d'`
-    analyze_tcp=`echo "${conn_info}"|awk -F '[: ]' '{arr[$1"|"$3"|"$5"|"]++}END{for(i in arr)print i arr[i]}' |sort -t '|' -k 4 -nr`
+    analyze_tcp=`echo "${conn_info}"|awk -F '[: ]' '{arr[$1"|"$2"|"$3"|"$5"|"]++}END{for(i in arr)print i arr[i]}' |sort -t '|' -k 4 -nr`
 }
 
 # varialb: top_10_info 
@@ -220,6 +220,7 @@ disk_info
 tcp_info
 processor_top_ten
 # 分析tcp连接情况输出到日志中
+log "$conn_info"
 log "$analyze_tcp"
 
 while true
